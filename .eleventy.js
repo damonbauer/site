3ea0399db8e9cdf0e1a00151c14f038bdfc041f9
Damon Bauer <damonbauer@users.noreply.github.com>
2020-04-22 20:33:10 -0500

    brutal (#4)
    * No more styles.

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,4 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const cleanCSS = require("clean-css");
-
-module.exports = eleventyConfig => {
-  eleventyConfig.addFilter("cssmin", (code) => 
-    new cleanCSS({}).minify(code).styles);
-
-  eleventyConfig.addPlugin(syntaxHighlight);
-  
+module.exports = eleventyConfig => {  
   return {
     htmlTemplateEngine: "liquid"
   };
